be475128c474fad0459e1938414c948f93172ace
Chris Desiderio <keanac08@gmail.com>
2018-02-19 16:58:42 +0800

    add interface program for tagged units uploading to security bank

---
+++ application/controllers/security_bank.php
@@ -0,0 +1,151 @@
+<?php 
+class Security_bank extends CI_Controller{
+	
+	public function __construct(){
+		parent::__construct();
+		$this->load->helper('file');
+		$this->load->model('security_bank_model');
+		//~ session_check();
+	}
+	
+	public function export_tagged(){
+		
+		$this->load->library('excel');
+		$this->load->helper('date_helper');
+		
+		$writer = new XLSXWriter();
+		
+		$header = array(
+						'CUSTOMER_ID' => 'string',
+						'INVOICE_NUMBER' => 'string',
+						'INVOICE_DATE' => 'MM/DD/YYYY',
+						'INVOICE_DUE_DATE' => 'MM/DD/YYYY',
+						'INVOICE_AMOUNT' => '0.00',
+						'DOCUMENT_TYPE' => 'string',
+						'COLLECTION_ACCT_NO' => 'string',
+						'MODEL' => 'string',
+						'SERIAL_NUMBER' => 'string',
+						'ENGINE_NUMBER' => 'string',
+						'CS_NUMBER' => 'string',
+						'YEAR_MODEL' => 'string',
+						'BODY_COLOR' => 'string',
+						'EXCEMPT' => '0.00',
+						'ZERO_RATED' => '0.00',
+						'DISCOUNT' => '0.00',
+						'NET_AMOUNT' => '0.00',
+						'PO_REF_NUMBER' => 'string',
+						'WB_NUMBER' => 'string',
+						'KEY_NUMBER' => 'string',
+						'VAT' => '0.00',
+						'ADDRESS1' => 'string',
+						'ADDRESS2' => 'string',
+						'ADDRESS3' => 'string',
+						'LOT_NUMBER' => 'string',
+						'WHT' => '0.00'
+					);
+		$writer->writeSheetHeader('Sheet1', $header );
+		
+		$text = 'BuyerCode~~InvoiceNumber~~InvoiceDate~~InvoiceDueDate~~InvoiceAmount~~DocumentType~~CollectionAcctNo~~Model~~SerialNo~~EngineNo~~Con_Sticker~~YearModel~~Color~~Excempt~~Zero Rated~~Discount~~NetAmount~~PORefNo~~WBNo~~KeyNo~~VAT~~DeliveryAddr1~~DeliveryAddr2~~DeliveryAddr3~~LotNumber~~WHoldTax~~dtl_fld1~!';
+		
+		$rows = $this->security_bank_model->get_tagged();
+		
+		$cnt = 0;
+		
+		foreach($rows as $row){
+			
+			$cnt++;
+			
+			$text .= '~'.$row->CUSTOMER_ID . '~~' . 
+				 $row->INVOICE_NUMBER . '~~' . 
+				 $row->INVOICE_DATE . '~~' . 
+				 $row->INVOICE_DUE_DATE . '~~' . 
+				 $row->INVOICE_AMOUNT . '~~' . 
+				 $row->DOCUMENT_TYPE . '~~' . 
+				 $row->COLLECTION_ACCT_NO . '~~' . 
+				 $row->MODEL . '~~' . 
+				 $row->SERIAL_NUMBER . '~~' . 
+				 $row->ENGINE_NUMBER . '~~' . 
+				 $row->CS_NUMBER . '~~' . 
+				 $row->YEAR_MODEL . '~~' . 
+				 $row->BODY_COLOR . '~~' . 
+				 $row->EXCEMPT . '~~' . 
+				 $row->ZERO_RATED . '~~' . 
+				 $row->DISCOUNT . '~~' . 
+				 $row->NET_AMOUNT . '~~' . 
+				 $row->PO_REF_NO . '~~' . 
+				 $row->WB_NUMBER . '~~' . 
+				 $row->KEY_NUMBER . '~~' . 
+				 $row->VAT . '~~' . 
+				 $row->ADDRESS1 . '~~' . 
+				 $row->ADDRESS2 . '~~' . 
+				 $row->ADDRESS3 . '~~' . 
+				 $row->LOT_NUMBER . '~~' . 
+				 $row->WHT . '~~~!';
+				 
+			$line = array(
+						$row->CUSTOMER_ID, 
+						$row->INVOICE_NUMBER, 
+						excel_date($row->INVOICE_DATE), 
+						excel_date($row->INVOICE_DUE_DATE), 
+						$row->INVOICE_AMOUNT, 
+						$row->DOCUMENT_TYPE, 
+						$row->COLLECTION_ACCT_NO, 
+						$row->MODEL, 
+						$row->SERIAL_NUMBER, 
+						$row->ENGINE_NUMBER, 
+						$row->CS_NUMBER, 
+						$row->YEAR_MODEL, 
+						$row->BODY_COLOR, 
+						$row->EXCEMPT, 
+						$row->ZERO_RATED, 
+						$row->DISCOUNT, 
+						$row->NET_AMOUNT, 
+						$row->PO_REF_NO, 
+						$row->WB_NUMBER, 
+						$row->KEY_NUMBER, 
+						$row->VAT, 
+						$row->ADDRESS1, 
+						$row->ADDRESS2, 
+						$row->ADDRESS3, 
+						$row->LOT_NUMBER, 
+						$row->WHT
+					);
+			$writer->writeSheetRow('Sheet1', $line);
+			$this->security_bank_model->insert_sent_tagged($row->CS_NUMBER);
+		}
+
+		if($cnt > 0){
+			$filename = 'securitybank';
+			
+			//~ write excel file
+			$writer->writeToFile($filename.'.xlsx');
+			
+			//~ write text file
+			$myfile = fopen($filename.'.txt', 'w');
+			fwrite($myfile, $text);
+			fclose($myfile);
+			
+			$this->load->library('emailerphp');
+			$mail = new EmailerPHP;
+			
+			$mail->addAddress('christopher-desiderio@isuzuphil.com');
+			$mail->addCC('eric-alcones@isuzuphil.com');
+			$mail->addCC('nathalie-baladad@isuzuphil.com');
+			$mail->addCC('zandra-dela-pena@isuzuphil.com');
+			
+			$mail->Subject = 'Security Bank - Tagged Units for Uploading';
+			$mail->AddAttachment($filename.'.xlsx');
+			$mail->AddAttachment($filename.'.txt');
+			$mail->Body = '<p>Please see attached newly tagged units for uploading.</p>';
+			$mail->Body .= '<p>&nbsp;</p>';
+			$mail->Body .= '<p>This is an automated message. Please do not respond to this e-mail.</p>';
+		
+			$mail->send();
+			
+			$mail = NULL;
+			
+			unlink($filename.'.xlsx');
+			unlink($filename.'.txt');
+		}
+	}
+}
---
+++ application/models/security_bank_model.php
@@ -0,0 +1,77 @@
+<?php
+
+class Security_bank_model extends CI_Model {
+	
+	public function __construct(){
+		
+		parent::__construct();
+		$this->oracle = $this->load->database('oracle', true);
+	}
+
+	public function get_tagged(){
+		
+		$sql = "SELECT hcca.cust_account_id customer_id,
+						msn.serial_number || '-' || TO_CHAR(msn.d_attribute20, 'MMDDYYYY') INVOICE_NUMBER,
+						to_char(d_attribute20, 'MM/DD/YYYY') invoice_date,
+						to_char(d_attribute20, 'MM/DD/YYYY') invoice_due_date,
+						round(oola.unit_selling_price + oola.tax_value,2) invoice_amount,
+						'INV' document_type,
+						'0222025472001' collection_acct_no,
+						msib.attribute9 model,
+						'NULL' serial_number,
+						msn.attribute3 engine_number,
+						msn.serial_number cs_number,
+						NVL(reverse(substr(reverse(substr(msn.attribute1,7)),-4)),EXTRACT(year FROM sysdate)) year_model,
+						msib.attribute8 body_color,
+						'0' excempt,
+						'0' zero_rated,
+						'0' discount,
+						-- CASE WHEN oola.unit_list_price - oola.unit_selling_price > 0 THEN  round(oola.unit_list_price - oola.unit_selling_price,2) ELSE 0 END discount,
+						ROUND(oola.unit_selling_price - (oola.unit_selling_price * .01),2) net_amount,
+						ooha.order_number po_ref_no,
+						'NULL' wb_number,
+						msn.attribute6 key_number,
+						round(oola.tax_value,2) vat,
+						CASE hp.address1 WHEN 'DEALERS-PARTS' THEN 'NULL' ELSE hp.address1 end address1,
+						hp.address2,
+						hp.address3,
+						msn.lot_number,
+						round(oola.unit_selling_price * .01,2) wht
+					FROM oe_order_headers_all ooha
+						 LEFT JOIN oe_order_lines_all oola 
+							ON ooha.header_id = oola.header_id
+						 LEFT JOIN mtl_reservations mr
+							ON oola.line_id = mr.demand_source_line_id
+						 LEFT JOIN mtl_serial_numbers msn
+							ON mr.reservation_id = msn.reservation_id
+						 LEFT JOIN hz_cust_accounts_all hcca
+							ON oola.sold_to_org_id = hcca.cust_account_id
+						 LEFT JOIN hz_parties hp ON hcca.party_id = hp.party_id
+						 LEFT JOIN mtl_system_items_b msib
+							ON oola.inventory_item_id = msib.inventory_item_id
+							   AND oola.ship_from_org_id = msib.organization_id
+						 LEFT JOIN oe_transaction_types_tl ottl
+							ON ooha.order_type_id = ottl.transaction_type_id
+						 LEFT JOIN ra_terms_tl rt 
+							ON oola.payment_term_id = rt.term_id
+						 LEFT JOIN oe_order_holds_all hold 
+							ON oola.line_id = hold.line_id
+				   WHERE 1 = 1
+						 AND oola.ship_from_org_id = 121
+						 AND hcca.cust_account_id = 15096
+						 AND msn.serial_number IS NOT NULL
+						 AND NVL (hold.RELEASED_FLAG, NVL (oola.ATTRIBUTE20, 'N')) = 'N'
+						 AND msn.serial_number NOT IN (SELECT cs_number FROM ipc.ipc_sb_tagged_units)";
+					   
+		$data = $this->oracle->query($sql);
+		return $data->result();
+		
+	}
+	
+	public function insert_sent_tagged($cs_number){
+		
+		$sql = "INSERT INTO ipc.ipc_sb_tagged_units (cs_number) VALUES (?)";
+		$this->oracle->query($sql, $cs_number);
+		
+	}
+}
